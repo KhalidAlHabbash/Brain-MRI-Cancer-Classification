4b955a7a26600db8ef399fb0543a0a265e56e29b
KhalidAlHabbash <khalidhabbash987@gmail.com>
2024-10-28 09:19:42 -0700

    Simplified and updated model architecture

--- backend/src/model.py
+++ backend/src/model.py
@@ -1,38 +1,35 @@
-
 import torch.nn as nn
 
 
 class BrainTumorClassifier(nn.Module):
     def __init__(self, num_classes):
         super(BrainTumorClassifier, self).__init__()
-        # First convolutional layer, takes 3 inputs (RGB)
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=4, stride=1, padding=0)
-        # Normalize the 32 output features from the first conv layer for stability during training
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=0)
         self.bn1 = nn.BatchNorm2d(32)
-        # Second conv layer, taking in the 32 output features of the first conv layer
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=1, padding=0)
-        # Normalize the 64 output features
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0)
         self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=4, stride=1, padding=0)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=0)
         self.bn3 = nn.BatchNorm2d(128)
-        self.conv4 = nn.Conv2d(128, 128, kernel_size=4, stride=1, padding=0)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=0)
         self.bn4 = nn.BatchNorm2d(128)
         self.pool = nn.MaxPool2d(kernel_size=3, stride=3)
         self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.fc1 = nn.Linear(6*6*128, 512)
+        self.fc1 = nn.Linear(8*8*128, 512)
         self.fc2 = nn.Linear(512, num_classes)
         self.flatten = nn.Flatten()
-        self.dropout = nn.Dropout(0.5)
         self.relu = nn.ReLU()
-
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
         """
         Forward pass of the CNN
         """
-        x = self.pool(self.relu(self.bn1(self.conv1(x))))
-        x = self.pool(self.relu(self.bn2(self.conv2(x))))
-        x = self.pool2(self.relu(self.bn3(self.conv3(x))))
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.pool(x)
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.pool(x)
+        x = self.relu(self.bn3(self.conv3(x)))
+        x = self.pool2(x)
         x = self.relu(self.bn4(self.conv4(x)))
         x = self.flatten(x)
         x = self.relu(self.fc1(x))
